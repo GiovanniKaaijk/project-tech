d83e559c9372fd36015615abb8f2ba6c4b3f25b5
GiovanniKaaijk <kaaijkg@gmail.com>
2019-05-16 15:14:40 +0200

    Added random cat api fetch

--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const express = require('express')
 const app = express()
 const port = 5500
 
+const fetch = require('node-fetch');
+let imgsrc = '';
+const randomCat = () => {
+    fetch('https://aws.random.cat/meow')
+    .then(res => res.json())
+    .then(json => {
+        imgsrc = json.file; 
+      });
+    };
+randomCat()
+
 app.set('view engine', 'pug')
 app.get('/', function (req, res) {
     res.render('index', {
         title: 'Home',
-        message: ''
+        message: '',
+        file: imgsrc
     })
+})
+app.use(express.static('public'), function(req,res){
+    res.status(404).render('404.pug', {
+        title: '404 error',
+        message: 'Page not found'
+    });
 })
 app.get('/about', function (req, res) {
     res.render('about', {
@@ -20,11 +38,5 @@ app.get('/login', function (req, res) {
         title: '-',
         message: ''
     })
-})
-app.use(express.static('public'), function(req,res){
-    res.status(404).render('404.pug', {
-        title: '404 error',
-        message: 'Page not found'
-    });
 })
 app.listen(port, () => console.log(`Server listening on port: ${port}`))
