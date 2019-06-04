822319a742c8e1b04907ab33956cb0386c742bba
GiovanniKaaijk <kaaijkg@gmail.com>
2019-06-04 15:48:25 +0200

    Added register connection to mongodb

--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,83 @@
-function routes(){
+const routes = require('express').Router()
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt');
+const user = require('../users/user')
+const urlencodedParser = bodyParser.urlencoded({extended:false});
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+routes.use(bodyParser.urlencoded({extended: false}));
+//placeholder while fetch is still loading
+let imgsrc = 'https:\/\/purr.objects-us-east-1.dream.io\/i\/20160628_130711.jpg';
 
-    app.get('/', function (req, res) {
-        res.render('index', {
-            title: 'Home',
-            message: '',
-            file: imgsrc
-        })
-    })
-    app.get('/about', function (req, res) {
-        res.render('about', {
-            title: '-',
-            message: ''
-        })
-    })
-    app.get('/login', function (req, res) {
-        res.render('login', {
-            title: '-',
-            message: ''
-        })
-    })
-}
+const randomCat = () => {
+fetch('https://aws.random.cat/meow')
+.then(res => res.json())
+.then(json => {
+    imgsrc = json.file; 
+    });
+};
 
-exports.routes = routes();
+
+routes.get('/', function (req, res) {
+    randomCat()
+    res.render('index', {
+        title: 'Home',
+        message: '',
+        file: imgsrc
+    })
+})
+
+routes.get('/login', function (req, res) {
+    res.render('login', {
+        title: '-',
+        message: ''
+    })
+})
+routes.get('/register', function (req, res) {
+    res.render('register', {
+        title: '-',
+        message: ''
+    })
+})
+
+//used https://github.com/Createdd/Writing/blob/9f6f202750d0b91a22ddf64f6c8e9a5f4b0caeb6/2017/articles/AuthenticationIntro.md
+
+routes.post('/register', (req, res) => {
+    console.log(req.body)
+    if (req.body.password == req.body.passwordconf) {
+        if (req.body.username && req.body.password) {
+            console.log(req.body)
+            let userData = {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
+            }
+            user.create(userData, (err, user) => {
+                if(err){
+                    console.log(err)
+                } else {
+                    return res.redirect('http://localhost:9090/')
+                }
+            })
+        } else {
+            console.log('Vul alle velden in')
+        }
+  } else {
+      console.log('password is niet gelijk')
+      return res.send('De wachtwoorden komen niet overeen')
+  }
+});
+
+
+
+routes.use(function(req,res){
+        res.status(404).render('404.pug', {
+            title: '404 error',
+            message: 'Page not found :('
+        });
+    })
+
+    
+module.exports = routes
 
--- server.js
+++ server.js
@@ -1,89 +1,31 @@
 const express = require('express')
 const app = express()
-const port = 5500
-
+const port = 9090
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch');
-
 const mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
-
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
 require('dotenv').config()
-
-
-// login request
-app.use(bodyParser.urlencoded({extended: false}));
-
-var data = [{
-    name: 'test'
-}];
-app.post('/api/data', (request, response) => {
-    const postBody = request.body;
-    console.log(postBody);
-    console.log(data)
-    data.push(postBody)
-    response.redirect('http://localhost:5500/')
-  });
 // multer
 
-// Mongodb
+// Mongodb (Code from cloud.mongodb.com)
 const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-   if(err){
-       console.log(err)
-   } else {
-       console.log('Connected to mongodb')
-   }
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect(uri, {useCreateIndex: true ,useNewUrlParser: true })
+    .then(() => console.log('connected'))
+    .catch(err => console.error('failed to connect', err))
 
 
-// catimg
-//placeholder while fetch is still loading
-let imgsrc = 'https:\/\/purr.objects-us-east-1.dream.io\/i\/20160628_130711.jpg';
-    
-const randomCat = () => {
-fetch('https://aws.random.cat/meow')
-.then(res => res.json())
-.then(json => {
-    imgsrc = json.file; 
-    });
-};
 
 //routes
+app.use(express.static('public'))
+app.use(require('./routes/routes'))
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: false,
+}));
 app.set('view engine', 'pug')
-app.get('/', function (req, res) {
-    randomCat()
-    res.render('index', {
-        title: 'Home',
-        message: '',
-        file: imgsrc,
-        data: data
-    })
-})
-
-app.get('/about', function (req, res) {
-    res.render('about', {
-        title: '-',
-        message: ''
-    })
-})
-app.get('/login', function (req, res) {
-    res.render('login', {
-        title: '-',
-        message: ''
-    })
-})
-
-
-app.use(express.static('public'), function(req,res){
-    res.status(404).render('404.pug', {
-        title: '404 error',
-        message: 'Page not found :('
-    });
-})
-
 app.listen(port, () => console.log(`Server is gestart op poort: ${port}`))
